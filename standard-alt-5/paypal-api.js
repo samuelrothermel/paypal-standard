8c1c77e194620a633c3427e8d6c8e8367e6e6dd0
Sam Rothermel <samuelrothermel@gmail.com>
2024-06-25 09:28:38 -0400

    committing another alt standard int

---
+++ standard-alt-5/paypal-api.js
@@ -0,0 +1,81 @@
+import fetch from "node-fetch";
+
+// set some important variables
+const { CLIENT_ID, APP_SECRET } = process.env;
+const base = "https://api-m.sandbox.paypal.com";
+
+// call the create order method
+export async function createOrder({ paymentSource }) {
+  const purchaseAmount = "9.99"; // TODO: pull prices from a database
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: purchaseAmount,
+          },
+        },
+      ],
+    }),
+  });
+ 
+  const data = await response.json();
+  console.log("Capture Request");
+  console.log(data);
+  return data;
+}
+
+// capture payment for an order
+export async function capturePayment(orderId) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+// generate access token
+export async function generateAccessToken() {
+  const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "post",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+  const data = await response.json();
+  return data.access_token;
+}
+
+// generate client token
+export async function generateClientToken(body) {
+  const accessToken = await generateAccessToken();
+  const request = {
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Accept-Language": "en_US",
+      "Content-Type": "application/json",
+    },
+  };
+  if (body) request.body = JSON.stringify(body);
+  const response = await fetch(`${base}/v1/identity/generate-token`, request);
+  const data = await response.json();
+  return data.client_token;
+}
